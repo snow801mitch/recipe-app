c0940819b4f6cf349544a42c86fb5227776d1030
Mitch Snow <snow801mitch@gmail.com>
2019-05-17 18:58:52 -0600

    fixed the style.recipe module on the div instead of the h1.

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,8 +3,8 @@ import style from './recipe.module.css';
 
 const Recipe = ({title, image, ingredients}) => {
     return(
-        <div>
-            <h1 className={style.recipe}>{title}</h1>
+        <div className={style.recipe}>
+            <h1>{title}</h1>
             <ul className="ingredient-list">
                 {ingredients.map(ingredient =>(
                     <li>{ingredient.text}</li>
